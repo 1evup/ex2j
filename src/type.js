02402bffd97858e182a877e2eb8fd6b0e69625e1
lev <levup2000@gmail.com>
2024-01-05 22:48:12 +0900

    add type token

---
+++ src/type.js
@@ -0,0 +1,28 @@
+const ValueType = {
+    Int: 'Int',
+    Float: 'Float',
+    Atom: 'Atom',
+    Range: 'Range',
+    Regex: 'Regex'
+};
+
+const SysteType = {
+
+}
+
+const CollectionType = {
+    Tuple: 'Tuple',
+    List: 'List',
+    Map: 'Map',
+    Binary: 'Binary'
+}
+
+const Boolean = {
+    True: 'True',
+    False: 'False',
+    Nil: 'nil'
+}
+
+const Operators = {
+
+}
